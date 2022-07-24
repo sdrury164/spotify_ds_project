b03f669fd066fb9745dfcc3a6f2ffbfed1820f88
sdrury164 <ubersteve164@gmail.com>
2022-07-24 13:55:33 -0400

    Adding file to load in credentials. My Spotify credentials have been removed for privacy reasons.

---
+++ spotify_credentials.py
@@ -0,0 +1,11 @@
+client_id = 'REDACTED'
+client_secret = 'REDACTED'
+redirect_url = 'REDACTED'
+
+client_id2 = 'REDACTED'
+client_secret2 = 'REDACTED'
+redirect_url2 = 'REDACTED'
+
+client_id3 = 'REDACTED'
+client_secret3 = 'REDACTED'
+redirect_url3 = 'REDACTED'
